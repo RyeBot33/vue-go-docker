7a8677988196d162de89a697de1d79956f41f6a2
Ryan Brothers <michael.brothers3333@gmail.com>
2020-09-14 14:36:56 -0500

    added vuetify

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
+//import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
 
-createApp(App).mount('#app')
+//createApp(App).mount('#app')
+new Vue({
+  vuetify,
+  render: h => h(App)
+}).$mount("#app");

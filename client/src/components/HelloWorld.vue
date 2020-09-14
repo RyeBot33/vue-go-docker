be8eca7bbad4f4c9ce9ec8ce468b0e90b7307326
Ryan Brothers <michael.brothers3333@gmail.com>
2020-09-14 15:20:25 -0500

    added vuetify starter template

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,30 +1,82 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-        <h1>{{ msg }}</h1>
-      <v-col class="mb-4">
-     </v-col>
-    </v-row>
-  </v-container>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-email</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="indigo"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col class="text-center">
+            <v-tooltip left>
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  :href="source"
+                  icon
+                  large
+                  target="_blank"
+                  v-on="on"
+                >
+                  <v-icon large>mdi-code-tags</v-icon>
+                </v-btn>
+              </template>
+              <span>Source</span>
+            </v-tooltip>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+    <v-footer
+      color="indigo"
+      app
+    >
+      <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import axios from 'axios'
   export default {
-    name: 'HelloWorld',
-    mounted() {
-      axios.get('/api').then(res => this.msg = res.data)
+    props: {
+      source: String,
     },
     data: () => ({
-      msg: null
+      drawer: null,
     }),
   }
 </script>
--- server/cmd/main.go
+++ server/cmd/main.go
@@ -7,27 +7,27 @@ import (
 	"net/http"
 )
 
-type Person struct{
+type Person struct {
 	FirstName string `json: "firstname"`
-	LastName string `json: "lastname"`
-	Email string `json:"email"`
-	Password string `json:"password"`
+	LastName  string `json: "lastname"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
 }
 
-func main(){
+func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/write", write)
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }
 
-func enableCors(w *http.ResponseWriter){
+func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 }
 
-func index(w http.ResponseWriter, r *http.Request){
+func index(w http.ResponseWriter, r *http.Request) {
 	enableCors(&w)
 	p := Person{
-		FirstName: "Ryan",
+		FirstName: "Fuck",
 		LastName:  "Brothers",
 		Email:     "test@test.com",
 		Password:  "JennaNen",
@@ -39,6 +39,6 @@ func index(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, string(bs))
 }
 
-func write(w http.ResponseWriter, r *http.Request){
+func write(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r)
 }
